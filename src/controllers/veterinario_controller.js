73b63d3721cd17de296651f79f299a2ace8090d2
elvissoide <elviserock@gmail.com>
2023-07-03 15:54:44 -0500

    Módulo de registro y envío de emails

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -1,11 +1,32 @@
+import Veterinario from "../models/Veterinario.js"
+import sendMailToUser from "../config/nodemailer.js"
+
 const login =(req,res)=>{
     res.status(200).json({res:'login del veterinario'})
 }
 const perfil=(req,res)=>{
     res.status(200).json({res:'perfil del veterinario'})
 }
-const registro =(req,res)=>{
-    res.status(200).json({res:'registro de un nuevo veterinario'})
+const registro = async (req,res)=>{
+    // Capturar los datos del body de la petición
+    const {email,password} = req.body
+    // Validación de los campos vacíos
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    // Verificar existencia del email
+    const verificarEmailBDD = await Veterinario.findOne({email})
+    if(verificarEmailBDD) return res.status(400).json({msg:"Lo sentimos, el email ya se encuentra registrado"})
+    // Crear la instancia del modelo
+    const nuevoVeterinario = new Veterinario(req.body)
+    // Encriptar el password del usuario
+    nuevoVeterinario.password = await nuevoVeterinario.encrypPassword(password)
+    // Crear el token para el usuario
+    const token = nuevoVeterinario.crearToken()
+    // Invocar la funcion para el envio del correo
+    await sendMailToUser(email,token)
+    // Guardar en BDD
+    await nuevoVeterinario.save()
+    // Enviar respuesta
+    res.status(200).json({msg:"Revisa tu correo electrónico para confirmar tu cuenta"})
 }
 const confirmEmail = (req,res)=>{
     res.status(200).json({res:'confirmar email de registro de veterinario'})
