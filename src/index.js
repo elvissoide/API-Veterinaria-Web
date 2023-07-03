010ae17b57eb4da30afa20ed4ba16bd88a91283e
elvissoide <elviserock@gmail.com>
2023-07-03 14:51:45 -0500

    Conexion a MongoDB Compass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // Importación de la varuable app por medio de módulos
 import app from './server.js'
+import connection from './database.js';
+
+connection()
 
 // Ejecutar el servidor por medio del puerto 3000
 app.listen(app.get('port'),()=>{
