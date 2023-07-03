4bcc25c9d47dc1d2e8045f2446bd8249fd3c9ac4
elvissoide <elviserock@gmail.com>
2023-07-03 15:10:19 -0500

    Controlador DB

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors';
+import routerVeterinarios from './routers/veterinario_routes.js'
 
 // Inicializaciones
 // Inicializar express en la variable app
@@ -22,6 +23,10 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send("Server on")
 })
+app.use('/api',routerVeterinarios)
+
+// Manejo de una ruta que no sea encontrada
+app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 
 // Exportacion por default
 export default  app
